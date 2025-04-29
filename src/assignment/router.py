520156bb91c90e9b78fe8a3b563295a5e4278486
Lucas <lucas97dias@outlook.com>
2025-04-29 23:01:55 +0100

    add submitted filter and submit action

--- src/assignment/router.py
+++ src/assignment/router.py
@@ -1,6 +1,10 @@
+from datetime import datetime, UTC
+
 from fastapi import APIRouter
 from sqlalchemy import select
 from sqlalchemy.orm import joinedload
+from starlette import status
+from starlette.responses import Response
 
 from assignment import Assignment
 from assignment.schema import AssignmentSchema
@@ -10,12 +14,31 @@ assignment_router = APIRouter()
 
 
 @assignment_router.get("/")
-async def list_assignments(session: SessionDep):
-    assignment_list = await session.scalars(
+async def list_assignments(session: SessionDep, show_expired: bool = False):
+    query = (
         select(Assignment)
         .options(
             joinedload(Assignment.task),
             joinedload(Assignment.scheduler)
         )
     )
-    return [AssignmentSchema.model_validate(a) for a in assignment_list]
+
+    if show_expired is False:
+        query = query.where(Assignment.due_datetime > datetime.now(UTC))
+
+    assignment_list = await session.scalars(query)
+    result = [AssignmentSchema.model_validate(a) for a in assignment_list]
+    return result
+
+
+@assignment_router.post("/{assignment_id}/submit")
+async def submit_assignment(session: SessionDep, assignment_id: int, response: Response):
+    assignment = await session.get(Assignment, assignment_id)
+
+    if assignment is None:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"msg": f"No assignment with id {assignment_id}"}
+
+    assignment.submission_datetime = datetime.now(UTC)
+    await session.commit()
+    return assignment.submission_datetime
--- src/tests/test_assignment/test_router.py
+++ src/tests/test_assignment/test_router.py
@@ -1,6 +1,9 @@
+from datetime import datetime, UTC, timedelta
+
 import pytest
 from starlette.testclient import TestClient
 
+from assignment import Assignment
 from assignment.router import assignment_router
 
 
@@ -10,12 +13,45 @@ def client(empty_app):
     return TestClient(empty_app)
 
 
+@pytest.fixture
+async def expired_assignment(session, schedule_in_db):
+    due_dt = datetime.now(UTC) - timedelta(hours=5)
+    a = Assignment(task=schedule_in_db.task, scheduler=schedule_in_db, due_datetime=due_dt)
+    session.add(a)
+    await session.commit()
+    return a
+
+
 class TestGet:
 
-    async def test_list_all(self, client, assignment_in_db):
+    def test_list_non_expired(self, client, assignment_in_db, expired_assignment):
         res = client.get("/")
 
+        assert res.status_code == 200
         data = res.json()
         assert len(data) == 1
         assert data[0]["id"] == assignment_in_db.id
+
+    def test_list_all(self, client, assignment_in_db, expired_assignment):
+        res = client.get("/", params={"show_expired": True})
+
+        assert res.status_code == 200
+        data = res.json()
+        assert len(data) == 2
+        assert data[0]["id"] == assignment_in_db.id
+
+
+class TestPost:
+
+    async def test_set_as_done(self, client, session, assignment_in_db):
+        res = client.post(f"/{assignment_in_db.id}/submit")
+
+        assert res.status_code == 200
+        await session.refresh(assignment_in_db)
+        assert assignment_in_db.submission_datetime is not None
+
+    def test_assignment_does_not_exists(self, client):
+        res = client.post(f"/9999/submit")
+
+        assert res.status_code == 404
 
